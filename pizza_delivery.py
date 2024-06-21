923d8382e8669c4d7d445a447d55ae6279bf004a
Nduduzo Ntshangase <101863087+nduduzontshangase@users.noreply.github.com>
2024-06-21 19:53:04 +0200

    Update pizza_delivery.py

--- pizza_delivery.py
+++ pizza_delivery.py
@@ -1,9 +1,9 @@
 # Pizza delivery
 
 print("Welcome to Python Pizza Deliveries!")
-size = input("What size pizza do you want? S, M or L ")
-add_pepperoni = input("Do you want pepperoni? Y or N ")
-extra_cheese = input("Do you want extra cheese? Y or N ")
+size = input("What size pizza would you like? S, M or L ")
+add_pepperoni = input("would you like pepperoni? Y or N ")
+extra_cheese = input("Would you like extra cheese? Y or N ")
 bill = 0
 
 if size == "S":
